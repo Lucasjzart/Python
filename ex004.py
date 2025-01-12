e604d701c7a2c2b7c49feed6f16a367c95326fb1
Lucasjzart <ljsdionisio@gmail.com>
2025-01-12 18:41:22 -0300

    Add files via upload

---
+++ ex004.py
@@ -0,0 +1,10 @@
+data = input('Digite algo: ')
+
+print('O tipo primitivo desse valor é', type(data))
+print('Só tem espaços?', data.isspace())
+print('É um número?', data.isnumeric())
+print('É alfabético?', data.isalpha())
+print('É alfanumérico?', data.isalnum())
+print('Está em maiúsculas?', data.isupper())
+print('Está em minúsculas?', data.islower())
+print('Está capitalizada?', data.istitle())
