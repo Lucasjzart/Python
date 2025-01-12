d4e1a14a973e9dfd8942a8e6a8f3aaef631a421a
Lucasjzart <ljsdionisio@gmail.com>
2025-01-12 18:27:16 -0300

    Add files via upload

---
+++ ex002.py
@@ -0,0 +1,8 @@
+n1 = int(input ("Digite o primeiro numero: "))
+print(type(n1))
+n2 = int(input ("Digite o segundo numero: "))
+print(type(n2))
+s = n1 + n2
+# print ("A soma entre", n1, "e", n2, "{}".format(s))
+print ("A soma entre {} e {} vale {}".format(n1, n2, s))
+print(type(s))
