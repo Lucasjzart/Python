dfd60857e27b1ac05dbb8a47f90a64541c3a32a2
Lucasjzart <ljsdionisio@gmail.com>
2025-01-31 19:40:36 -0300

    Update desafio006.py

--- desafio006.py
+++ desafio006.py
@@ -1,5 +1,2 @@
 numero = int(input("Digite um numero"))
-dobro = numero * 2
-triplo = numero * 3
-raiz = numero * (1/2)
-print (f"O dobro de {dobro} o triplo de {triplo} e a raiz é {raiz}")
+print (f"O dobro de {numero*2} o triplo de {numero*3} e a raiz é {numero*(1/2)}")
